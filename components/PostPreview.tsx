cd8191758eaee6072b7ab6de3fc14d8484c3c142
Angel Esqueda <68274444+noobato1@users.noreply.github.com>
2022-12-29 10:59:58 -0600

    Update PostPreview.tsx
    Change color link

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -5,7 +5,7 @@ const PostPreview = (props: PostMetadata) => {
   return (
     <div className="shadow-md bg-white p-6 border rounded-xl border-slate-200">
       <Link href={`posts/${props.slug}`}>
-        <h2 className="text-2xl text-violet-700 font-bold hover:underline">{props.title}</h2>
+        <h2 className="text-2xl text-pink-600 font-bold hover:underline">{props.title}</h2>
       </Link>
       <p className="text-sm text-slate-400">{props.date}</p>
       <p className="text-slate-800">{props.subtitle}</p>
